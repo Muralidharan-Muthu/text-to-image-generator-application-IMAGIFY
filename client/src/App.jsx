40c460b8117e29c31f4d28bbf823d2791f607b33
Muralidharan <muralimuthu047@gmail.com>
2025-05-15 21:35:14 +0530

    Added Icons in the assets folder and modified some files and finally added routing in the App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Result from './pages/Result';
+import BuyCredit from './pages/BuyCredit';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/result' element={<Result />}/>
+        <Route path='/buycredit' element={<BuyCredit />}/>
+      </Routes>
+    </div>
   )
 }
 
